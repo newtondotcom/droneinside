d24e07c9cf7640eb3a5afebdec162cbe7e6de683
newtondotcom <asphalt9fr@gmail.com>
2025-03-30 23:31:11 +0200

    chore: refactor links

---
+++ src/data/links.ts
@@ -0,0 +1,8 @@
+const links = [
+  { path: "/", label: "home" },
+  { path: "/services", label: "services" },
+  { path: "/portfolio", label: "portfolio" },
+  { path: "/faq", label: "faq" },
+];
+
+export default links;

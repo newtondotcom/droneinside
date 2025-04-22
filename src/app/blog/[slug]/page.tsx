bcc5a8ee215e83f23612c22b4b41d0279637c206
newtondotcom <asphalt9fr@gmail.com>
2025-04-22 15:09:33 +0000

    style: enhance blog page display

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 import { Suspense } from "react";
 import { formatDistanceToNow } from 'date-fns';
 import { fr } from 'date-fns/locale';
+import Link from "next/link";
 
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -67,7 +68,29 @@ export default async function Blog({ params}: BlogPageProps) {
   }
 
   return (
-    <section id="blog">
+    <section id="blog" className="flex flex-col justify-center items-center mx-auto min-h-screen py-12 w-full">
+      <h1 className="title font-medium text-2xl tracking-tighter max-w-[650px]">
+        {post.metadata.title}
+      </h1>
+      <div className="flex flex-col justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
+        <Suspense fallback={<p className="h-5" />}>
+          <p className="text-sm text-neutral-600 dark:text-neutral-400">
+            {formatDistanceToNow(new Date(post.metadata.publishedAt), {
+              addSuffix: true,
+              locale: fr,
+            })}
+          </p>
+        </Suspense>
+      <Link href="/blog" className="text-sm text-neutral-400 dark:text-neutral-600 underline">
+            Voir tous les articles
+      </Link>
+      </div>
+      <article
+        className="prose-sm md:prose dark:prose-invert"
+        dangerouslySetInnerHTML={{ __html: post.source }}
+      />
+
+            
       <script
         type="application/ld+json"
         suppressHydrationWarning
@@ -90,23 +113,6 @@ export default async function Blog({ params}: BlogPageProps) {
           }),
         }}
       />
-      <h1 className="title font-medium text-2xl tracking-tighter max-w-[650px]">
-        {post.metadata.title}
-      </h1>
-      <div className="flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
-        <Suspense fallback={<p className="h-5" />}>
-          <p className="text-sm text-neutral-600 dark:text-neutral-400">
-            {formatDistanceToNow(new Date(post.metadata.publishedAt), {
-              addSuffix: true,
-              locale: fr,
-            })}
-          </p>
-        </Suspense>
-      </div>
-      <article
-        className="prose dark:prose-invert"
-        dangerouslySetInnerHTML={{ __html: post.source }}
-      ></article>
     </section>
   );
 }

961f6aa40a11d0ae35e5832821cc82d43bba031f
newtondotcom <asphalt9fr@gmail.com>
2025-04-28 13:22:21 +0000

    fix: wrong redirect link

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -51,7 +51,7 @@ const Services = () => {
 
               <div className="mx-auto my-8 max-w-lg text-center text-base font-semibold text-neutral-700 dark:text-neutral-200">
                 {translate("services_redirect")}{" "}
-                <a href="/faq" className="text-primary underline">
+                <a href="/portfolio" className="text-primary underline">
                   {translate("here_button")}
                 </a>
               </div>

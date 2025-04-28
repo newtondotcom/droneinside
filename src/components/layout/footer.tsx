8e5c331a1e4d256710efe92e536812f4e1a7cce1
newtondotcom <asphalt9fr@gmail.com>
2025-04-28 12:22:30 +0000

    feat: bring back footer icons

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -6,15 +6,35 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Logo from "@/components/ui/logo";
 import { Separator } from "@/components/ui/separator";
+import { siGithub, siInstagram, siLinkedin } from 'simple-icons';
 
 const Footer = () => {
   const pathname = usePathname();
-  if (pathname == "/") {
+  if (pathname === "/") {
     return <></>;
   }
+
+  const socialLinks = [
+    {
+      href: "https://instagram.com/droninside",
+      label: "Instagram",
+      icon: <Instagram />,
+    },
+    {
+      href: "https://www.linkedin.com/in/robin-augereau/",
+      label: "LinkedIn",
+      icon: <LinkedIn />,
+    },
+    {
+      href: "https://github.com/newtondotcom/droninside",
+      label: "GitHub",
+      icon: <Github />,
+    },
+  ];
+
   return (
     <footer className="flex flex-col snap-none snap-align-none">
-      < Separator />
+      <Separator />
       <div className="mx-auto max-w-5xl mt-4 px-4 py-16 sm:px-6 lg:px-8">
         <div className="flex justify-center">
           <Logo />
@@ -46,24 +66,8 @@ const Footer = () => {
         </div>
 
         <ul className="mt-12 flex justify-center gap-6 md:gap-8">
-          {[
-            {
-              href: "https://instagram.com/droninside",
-              label: "Instagram",
-              icon: "instagram",
-            },
-            {
-              href: "https://www.linkedin.com/in/robin-augereau/",
-              label: "LinkedIn",
-              icon: "linkedin",
-            },
-            {
-              href: "https://github.com/newtondotcom/drone-shop.git",
-              label: "GitHub",
-              icon: "github",
-            },
-          ].map(({ href, label, icon }) => (
-            <li key={icon}>
+          {socialLinks.map(({ href, label, icon }) => (
+            <li key={label}>
               <a
                 href={href}
                 rel="noreferrer"
@@ -71,7 +75,14 @@ const Footer = () => {
                 className="text-gray-400 transition hover:text-primary"
                 aria-label={label}
               >
-                <i className={`fab fa-${icon} text-xl`}></i>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  {icon}
+                </svg>
               </a>
             </li>
           ))}
@@ -82,3 +93,29 @@ const Footer = () => {
 };
 
 export default Footer;
+
+
+function LinkedIn(){
+  return (
+    <>
+    <title>LinkedIn</title>
+    <path
+        d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"
+    />
+    </>
+  )
+}
+
+function Github(){
+  const path = siGithub.path;
+  return (
+      <path d={path} />
+  )
+}
+
+function Instagram(){
+  const path = siInstagram.path;
+  return (
+      <path d={path} />
+  )
+}
